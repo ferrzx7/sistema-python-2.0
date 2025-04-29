715b12f0142d44b7d8fa556f4c3437a79e95acfa
ferrzx <00001129651939sp@al.educacao.sp.gov.br>
2025-04-29 10:19:09 -0300

    criando funçoens

---
+++ function.py
@@ -0,0 +1,27 @@
+def soma() :
+    nun_01 = 25
+    nun_02 = 55
+
+    resultado = nun_01 + nun_02
+
+    return resultado
+
+print(soma())
+
+def somarDoisNumeros(nota_01, nota_02):
+    num_01 = nota_01
+    num_02 = nota_02
+
+    resultado = num_01 + num_02
+
+    return print(resultado)
+
+#somarDoisNumeros(29, 68)
+
+def multiplicarDoisNumeros(nota_01, nota_02):
+    num_01 = nota_01
+    num_02 = nota_02
+
+    resultado = num_01 * num_02
+    
+    return print(resultado)
---
+++ sistema.py
@@ -0,0 +1,6 @@
+nota01 = 6
+nota02 = 22
+
+soma = nota01 + nota02
+
+print(soma)
